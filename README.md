63d2a9fee450889e830cf65832d61b51ba09b8a0
ningenMe <amohkuru@gmail.com>
2022-01-10 00:42:20 +0900

    [add] db

---
+++ README.md
@@ -0,0 +1,8 @@
+# spring-boot-api-authorization-sample
+
+
+まずmysqlを立ち上げる
+```shell
+cd tool
+docker-compose up
+```
--- tool/schema.sql
+++ tool/schema.sql
@@ -1,8 +1,28 @@
-CREATE TABLE `users` (
-  `id`           integer(10) NOT NULL,
-  `name`         varchar(255) NOT NULL,
-  `deleted_time` timestamp NULL DEFAULT NULL,
-  `created_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
+CREATE TABLE `user` (
+  `user_id`    integer(10) NOT NULL,
+  `api_token`  varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY (`api_token`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `role` (
+                         `role_id`    integer(10) NOT NULL,
+                         `role_name`  varchar(255) NOT NULL,
+                         `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                         `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+                         PRIMARY KEY (`role_id`),
+                         UNIQUE KEY (`role_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `j_user_role` (
+                               `user_id`    integer(10) NOT NULL,
+                               `role_id`    integer(10) NOT NULL,
+                               `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                               `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+                               PRIMARY KEY (`user_id`,`role_id`)
+);
+
+INSERT INTO user (user_id,api_token) VALUES (1,'WDGL7mGX4j'),(2,'CT/FrUY667');
+INSERT INTO role (role_id,role_name) VALUES (1,'test1'),(2,'test2'),(3,'test3');
